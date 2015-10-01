052f972784a5704e0d10d706e4aff9f85268957f
Jason Cheng <jasoncheng@live.com.my>
2015-10-02 00:35:23 +0800

    Restful Api Update 1

--- modules/restfulApi.js
+++ modules/restfulApi.js
@@ -1,38 +1,45 @@
 'use strict';
 
 var
-	async = require('async');
+	apis = {}
+	, resourceConstructors = {}
+	, initializeResourceConstructors = function (resourceName) {
+		if (!(resourceName in resourceConstructors)) {
+			resourceConstructors[resourceName] = function () {
 
-var RestfulApi = function () { // constructor
-
-	var
-		permissions = [];
-
-	this.restful = function (collectionName) { // create middleware (external api)
-
-		return function (req, res) {
-
-			async.each(permissions, function (permission, done) { // execute permission callbacks
-				permission(req, done);
-			}, function (err) {
-				if (err) {
-
-				}
-			});
-
+			};
+			resourceConstructors[resourceName].prototype.permissions = {};
 		}
+	};
 
+// API Internal Methods
+apis.setResourcePermission = function (resourceName, methodName, permissionCallback) {
+	initializeResourceConstructors(resourceName);
+	if (!resourceConstructors[resourceName].prototype.permissions[methodName]) {
+		resourceConstructors[resourceName].prototype.permissions[methodName] = [];
 	}
+	resourceConstructors[resourceName].prototype.permissions[methodName].push(permissionCallback);
+}
 
-	this.setPermission = function (permissionCallback) { // store permission callbacks
-		permissions.push(function (req, done) {
-			permissionCallback(req, done);
-		});
-	}
+apis.setResourceMethod = function (resourceName, methodName, methodCallback) {
+	initializeResourceConstructors(resourceName);
+	console.log(methodName);
+	resourceConstructors[resourceName].prototype[methodName] = methodCallback;
+}
 
-};
+// testing
+apis.setResourcePermission('default', 'get', function (req, done) {
+	setTimeout(function () {
+		console.log('aloha');
+		return done();
+	}, 500);
+});
 
+apis.setResourceMethod('default', 'get', function (resourceName, res) {
+	setTimeout(function () {
+		console.log('aloha');
+		return res.json('bye');
+	}, 500);
+});
 
-
-
-module.exports = new RestfulApi();
+module.exports = apis;
