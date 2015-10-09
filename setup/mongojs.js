53896eb55cb1e0488a6a436570cf85a9e562e1dd
Jason Cheng <qishen.cheng@ivisgroup.com>
2015-10-09 09:50:08 +0800

    Added error and ready event handlers to mongojs

--- setup/mongojs.js
+++ setup/mongojs.js
@@ -3,6 +3,16 @@
 var
 	mongojs = require('mongojs')
 	, databaseUrl = require('../config')['mongodb.databaseUrl']
-	, db = mongojs(databaseUrl, ['User', 'Profile']);
+	, db = mongojs(databaseUrl, ['User', 'Profile'])
+	, logger = require('../modules/logger');
+
+db.on('error', function (err) {
+	logger.error(err);
+	console.log(err);
+});
+
+db.on('ready', function () {
+	console.log('Connected to mongo db');
+});
 
 module.exports = db;
