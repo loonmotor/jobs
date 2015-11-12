c5df25b5a92a518b28b59740921c5427ee9d6a49
Jason Cheng <qishen.cheng@ivisgroup.com>
2015-11-12 12:02:43 +0800

    Added indices to mongo deployment script

--- deployment/mongodb.setupIndices.js
+++ deployment/mongodb.setupIndices.js
@@ -6,3 +6,13 @@ db.User.ensureIndex({ 'name' : 1 });
 db.User.ensureIndex({ 'google.id' : 1 });
 db.User.ensureIndex({ 'facebook.id' : 1 });
 db.User.ensureIndex({ 'local.username' : 1 });
+
+db.Company.ensureIndex({ 'userId' : 1, 'modified' : 1 });
+
+db.Profile.ensureIndex({ 'userId' : 1 });
+
+db.Job.ensureIndex({ 'companyId' : 1 });
+db.Job.ensureIndex({ 'modified' : 1 });
+db.Job.ensureIndex({ 'archived' : 1 });
+db.Job.ensureIndex({ 'interests.userId' : 1 });
+db.Job.ensureIndex({ 'userId' : 1 });
