3bebe3605e65dbc57ed0ec397c3d3dfb56ae8119
Jason Cheng <qishen.cheng@ivisgroup.com>
2015-11-17 16:30:51 +0800

    Update mongo create indices deployment script

--- deployment/mongodb.setupIndices.js
+++ deployment/mongodb.setupIndices.js
@@ -2,17 +2,21 @@ var
 	mongo = new Mongo()
 	, db = mongo.getDB('jobs');
 
-db.User.ensureIndex({ 'name' : 1 });
-db.User.ensureIndex({ 'google.id' : 1 });
-db.User.ensureIndex({ 'facebook.id' : 1 });
-db.User.ensureIndex({ 'local.username' : 1 });
+db.User.dropIndexes();
+db.User.createIndex({ 'name' : 1 });
+db.User.createIndex({ 'google.id' : 1 }, { unique : true, sparse : true });
+db.User.createIndex({ 'facebook.id' : 1 }, { unique : true, sparse : true });
+db.User.createIndex({ 'local.username' : 1 }, { unique : true, sparse : true });
 
-db.Company.ensureIndex({ 'userId' : 1, 'modified' : 1 });
+db.Company.dropIndexes();
+db.Company.createIndex({ 'userId' : 1, 'modified' : 1 });
 
-db.Profile.ensureIndex({ 'userId' : 1 });
+db.Profile.dropIndexes();
+db.Profile.createIndex({ 'userId' : 1 });
 
-db.Job.ensureIndex({ 'companyId' : 1 });
-db.Job.ensureIndex({ 'modified' : 1 });
-db.Job.ensureIndex({ 'archived' : 1 });
-db.Job.ensureIndex({ 'interests.userId' : 1 });
-db.Job.ensureIndex({ 'userId' : 1 });
+db.Job.dropIndexes();
+db.Job.createIndex({ 'companyId' : 1 });
+db.Job.createIndex({ 'modified' : 1 });
+db.Job.createIndex({ 'archived' : 1 });
+db.Job.createIndex({ 'interests.userId' : 1 });
+db.Job.createIndex({ 'userId' : 1 });
